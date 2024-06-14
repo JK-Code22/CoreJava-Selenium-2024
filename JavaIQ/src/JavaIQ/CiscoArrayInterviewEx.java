e4a3006ce82d5b6cd928ad08e6748f519de3cd87
858679 <858679@LTIN541609.cts.com>
2024-06-14 10:53:13 +0100

    Test Commit

--- JavaIQ/src/JavaIQ/CiscoArrayInterviewEx.java
+++ JavaIQ/src/JavaIQ/CiscoArrayInterviewEx.java
@@ -1,33 +1,28 @@
 package JavaIQ;
 
-
-
 public class CiscoArrayInterviewEx {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int abc[][] = {{1,2,3},{5,9,6},{7,1,0}};
+		int abc[][] = { { 1, 2, 3 }, { 5, 9, 6 }, { 7, 0, 2 } };
+
 		int min = abc[0][0];
-		int minColumn =0;
-		
-		
-		for(int i=0; i<3; i++)
-		{
-			for(int j=0; j<3; j++)
-			{
-				if(abc[i][j]<min) 
-				{
+		int minColumn = 0;
+
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				if (abc[i][j] < min) {
 					min = abc[i][j];
 					minColumn = j;
-					
-			}	
-		}
 
-	}
+				}
+			}
+
+		}
 		int max = abc[0][0];
-		int k=0;
-		while(k<3) {
-			if(abc[k][minColumn] > max) {
+		int k = 0;
+		while (k < 3) {
+			if (abc[k][minColumn] > max) {
 				max = abc[k][minColumn];
 			}
 			k++;
